4d6bfc437e45787126930924971b6f08ddecf21a
Piotr Gródek <piotr.grodek@deepsense.ai>
2023-11-02 18:28:01 +0100

    Simple wrapper around docker API
    * fix linter settings

--- src/ds_pycontain/__init__.py
+++ src/ds_pycontain/__init__.py
@@ -1,4 +1,5 @@
-""" ds_pycontain """
+""" ds_pycontain is a Python package for managing Docker containers and images. """
 from .__version__ import __version__
+from .docker_containers import DockerContainer, DockerImage, generate_random_container_tag, get_docker_client
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "DockerContainer", "DockerImage", "generate_random_container_tag", "get_docker_client"]
